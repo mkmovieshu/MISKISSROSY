a53e2fb34c9177fcebf771bd85acec1ced328dd9
mkmovieshu <ipsappdevelopers@gmail.com>
2026-02-16 19:51:38 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+BOT_TOKEN = "మీ_బాట్_టోకెన్_ఇక్కడ_ఇవ్వండి"
+PREMIUM_CHANNEL_ID = -100123456789  # మీ ప్రీమియం ఛానల్ ID
+ADMIN_ID = 123456789  # మీ టెలిగ్రామ్ యూజర్ ID
+ADMIN_USERNAME = "@YourUsername"  # మీ యూజర్ నేమ్
+FREE_CHANNEL_LINK = "https://t.me/YourFreeChannel" # ఉచిత ఛానల్ లింక్
