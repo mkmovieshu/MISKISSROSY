9c77a22bcdf2f7ca7469a804a14c537d40d20aa5
mkmovieshu <ipsappdevelopers@gmail.com>
2026-02-16 20:00:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
-BOT_TOKEN = "మీ_బాట్_టోకెన్_ఇక్కడ_ఇవ్వండి"
-PREMIUM_CHANNEL_ID = -100123456789  # మీ ప్రీమియం ఛానల్ ID
-ADMIN_ID = 123456789  # మీ టెలిగ్రామ్ యూజర్ ID
-ADMIN_USERNAME = "@YourUsername"  # మీ యూజర్ నేమ్
-FREE_CHANNEL_LINK = "https://t.me/YourFreeChannel" # ఉచిత ఛానల్ లింక్
+import os
+
+# Render లో సెట్ చేసే ఎన్విరాన్మెంటల్ వేరియబుల్స్ నుండి డేటా తీసుకుంటుంది
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+PREMIUM_CHANNEL_ID = int(os.getenv("PREMIUM_CHANNEL_ID", "0"))
+ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
+ADMIN_USERNAME = os.getenv("ADMIN_USERNAME")
+FREE_CHANNEL_LINK = os.getenv("FREE_CHANNEL_LINK")
