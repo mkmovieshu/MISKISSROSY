eeb560e8a009d59cd306b96dc6e88ade59fd5a02
mkmovieshu <ipsappdevelopers@gmail.com>
2026-02-16 20:05:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,8 @@
 import os
 
-# Render Environment Variables నుండి డేటా తీసుకుంటుంది
+# Render Environment Variables
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-PREMIUM_CHANNEL_ID = int(os.getenv("PREMIUM_CHANNEL_ID", "0"))
-LOG_CHANNEL_ID = int(os.getenv("LOG_CHANNEL_ID", "0")) # కొత్త యూజర్ల వివరాల కోసం
+LOG_CHANNEL_ID = int(os.getenv("LOG_CHANNEL_ID", "0"))
 ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
 ADMIN_USERNAME = os.getenv("ADMIN_USERNAME")
 FREE_CHANNEL_LINK = os.getenv("FREE_CHANNEL_LINK")
