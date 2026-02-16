3a6587e91c66e0d6f79e0cbebdf0053ba56de331
mkmovieshu <ipsappdevelopers@gmail.com>
2026-02-16 20:11:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,4 +5,10 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 LOG_CHANNEL_ID = int(os.getenv("LOG_CHANNEL_ID", "0"))
 ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
 ADMIN_USERNAME = os.getenv("ADMIN_USERNAME")
-FREE_CHANNEL_LINK = os.getenv("FREE_CHANNEL_LINK")
+
+# సిరీస్ వివరాలు (పేరు, ధర, ఛానల్ ఐడి)
+SERIES_LIST = {
+    "series_1": {"name": "మహాభారతం ఆడియో", "price": "₹99", "cid": "-100123456789"},
+    "series_2": {"name": "రామాయణం ఆడియో", "price": "₹149", "cid": "-100987654321"},
+    "series_3": {"name": "కథల సిరీస్", "price": "₹49", "cid": "-100456123789"}
+}
