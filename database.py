82dda81cb7e18d2117cd5146fd8d85f585c8d880
mkmovieshu <ipsappdevelopers@gmail.com>
2026-02-16 20:22:47 +0530

    Update database.py

--- database.py
+++ database.py
@@ -5,11 +5,28 @@ client = MongoClient(MONGO_URL)
 db = client['telegram_bot_db']
 users_col = db['users']
 
-def add_user(user_id):
-    # యూజర్ లేకపోతేనే యాడ్ చేస్తుంది
-    if not users_col.find_one({"user_id": user_id}):
-        users_col.insert_one({"user_id": user_id})
+def add_user(user_id, referred_by=None):
+    user = users_col.find_one({"user_id": user_id})
+    if not user:
+        new_user = {
+            "user_id": user_id,
+            "referred_by": referred_by,
+            "bonus_episodes": 1 if referred_by else 0, # రిఫరల్ ద్వారా వస్తే 1 బోనస్
+            "referral_count": 0
+        }
+        users_col.insert_one(new_user)
+        # ఎవరైతే రిఫర్ చేశారో వారికి కూడా ఒక బోనస్ ఇవ్వడం
+        if referred_by:
+            users_col.update_one({"user_id": referred_by}, {"$inc": {"bonus_episodes": 1, "referral_count": 1}})
+        return True
+    return False
+
+def get_user_bonus(user_id):
+    user = users_col.find_one({"user_id": user_id})
+    return user.get("bonus_episodes", 0) if user else 0
+
+def use_bonus(user_id):
+    users_col.update_one({"user_id": user_id}, {"$inc": {"bonus_episodes": -1}})
 
 def get_all_users():
-    users = users_col.find({}, {"user_id": 1})
-    return [u['user_id'] for u in users]
+    return [u['user_id'] for u in users_col.find({}, {"user_id": 1})]
