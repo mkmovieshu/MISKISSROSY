34db8d93c5167479ec432c31ba55c5a2bae69bf7
mkmovieshu <ipsappdevelopers@gmail.com>
2026-06-22 22:49:10 +0530

    Update userdb.py

--- database/userdb.py
+++ database/userdb.py
@@ -1,8 +1,8 @@
 from motor.motor_asyncio import AsyncIOMotorClient
-from vars import MONGO_URL, MONGO_DB_NAME
+from vars import MONGO_URI
 
-mongo = AsyncIOMotorClient(MONGO_URL)
-db = mongo[MONGO_DB_NAME]
+mongo = AsyncIOMotorClient(MONGO_URI)
+db = mongo["adultzonebot"]
 
 users = db["users"]
 ad_sessions = db["ad_sessions"]
